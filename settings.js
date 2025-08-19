fbb8188339436d951b755388350e9f2ad94968d0
helooksatmeandilookhim-cmyk <helooksatmeandilookhim@gmail.com>
2025-08-19 04:43:47 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Leo', // Your name
-  ownerNumber: '1557342712', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '201557342712', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
